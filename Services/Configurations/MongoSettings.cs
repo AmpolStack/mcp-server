954a50df4ccc8259d72b6de2606090f8816a1a37
Amapola <htrsanbl@gmail.com>
2025-04-13 20:16:10 -0500

    feat: include services library

---
+++ Services/Configurations/MongoSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace Services.Configurations;
+
+public class MongoSettings
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Host { get; set; }
+    public string? Port { get; set; }
+    public string? Database { get; set; }
+
+    public string GetConnectionString()
+    {
+        return $"mongodb://{Username}:{Password}@{Host}:{Port}";
+    }
+    
+}
---
+++ Services/Definitions/UserDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Services.Definitions;
+
+public class UserDbContext
+{
+    
+}
