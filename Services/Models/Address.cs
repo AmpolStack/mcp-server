7ccbb6fde7b075f838127910edae21ab180dc365
Amapola <htrsanbl@gmail.com>
2025-04-13 22:47:23 -0500

    fix: include the Bson metatags, because is needded include the ID entity, and mongo db discriminates between upper and lower case

--- Services/Models/Address.cs
+++ Services/Models/Address.cs
@@ -1,9 +1,19 @@
-﻿namespace Services.Models;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Services.Models;
 
 public class Address
 {
+    
+    [BsonElement("street")]
     public string? Street { get; set; }
+    
+    [BsonElement("city")]
     public string? City { get; set; }
+    
+    [BsonElement("state")]
     public string? State { get; set; }
+    
+    [BsonElement("zipCode")]
     public string? ZipCode { get; set; }
 }
--- Services/Models/Client.cs
+++ Services/Models/Client.cs
@@ -1,13 +1,33 @@
-﻿namespace Services.Models;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Services.Models;
 
 public class Client
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
+    
+    [BsonElement("firstName")]
     public string? FirstName { get; set; }
+    
+    [BsonElement("lastName")]
     public string? LastName { get; set; }
+    
+    [BsonElement("email")]
     public string? Email { get; set; }
+    
+    [BsonElement("phone")]
     public string? Phone { get; set; }
+    
+    [BsonElement("address")]
     public Address? Address { get; set; }
+    
+    [BsonElement("isActive")]
     public bool IsActive { get; set; }
+    
+    [BsonElement("createdAt")]
     public DateTime CreatedAt { get; set; }
 
 }
