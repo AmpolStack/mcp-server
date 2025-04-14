fd65d497c48f592e6cf58bb4e04a008c9ce4a1bf
Amapola <htrsanbl@gmail.com>
2025-04-13 20:27:42 -0500

    include models

---
+++ Services/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace Services.Models;
+
+public class Client
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public Address? Address { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+}
