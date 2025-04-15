098d981f8ac8043b2fde865c46288669aee89c13
Amapola <htrsanbl@gmail.com>
2025-04-14 19:37:53 -0500

    add implementation of pdf generator

---
+++ Services/Implementations/PdfGeneratorService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+using Services.Definitions;
+
+namespace Services.Implementations;
+
+public class PdfGeneratorService : IPdfGeneratorService
+{
+    private readonly ILogger<PdfGeneratorService> _logger;
+
+    public PdfGeneratorService(ILoggerFactory loggerFactory)
+    {
+        this._logger = loggerFactory.CreateLogger<PdfGeneratorService>();
+    }
+
+
+    public async Task<bool> ConvertHtmlStringToPdf(string htmlContent, string outputPath)
+    {
+        try
+        {
+            await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions() { Headless = true });
+            await using var page = await browser.NewPageAsync();
+            await page.SetContentAsync(htmlContent);
+            await page.PdfAsync(outputPath);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to generate pdf from HTML string");
+            return false;
+        }
+    }
+}
