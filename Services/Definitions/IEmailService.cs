dccc685e078c92ae64eaec59b0fbf068b46650aa
Amapola <htrsanbl@gmail.com>
2025-04-15 11:54:32 -0500

    refactor(emailService): Added implementation of builder pattern

--- Services/Definitions/IEmailService.cs
+++ Services/Definitions/IEmailService.cs
@@ -1,8 +1,17 @@
-﻿using Services.Configurations;
+﻿using MimeKit;
+using Services.Configurations;
 
 namespace Services.Definitions;
 
 public interface IEmailService
 {
+    public void SetSenderEmail(string username, string address);
+    public void AddReceiverAddress(string username, string address);
+    public void SetMessageBody(string content);
+    public void SetMessageSubject(string content);
+    public void SetMessage(string subject, string body);
+    public void AddFile(string path, string subType);
+    public void AddFile(string path, string mediaType, string subType, ContentEncoding encoding);
+    public Task<bool> BuildAndSendAsync();
     public Task<bool> SendEmailAsync(string subject, string body, string toName, string toAddress, string path, SmtpServerConfiguration config);
 }
--- Services/Implementations/EmailService.cs
+++ Services/Implementations/EmailService.cs
@@ -1,21 +1,81 @@
-﻿using MailKit.Net.Smtp;
+﻿using System.Text;
+using MailKit.Net.Smtp;
 using Microsoft.Extensions.Logging;
 using MimeKit;
 using Services.Configurations;
+using Services.Custom;
 using Services.Definitions;
 
 namespace Services.Implementations;
 
 public class EmailService : IEmailService
 {
-    private ILogger<EmailService> _logger;
+    private readonly ILogger<EmailService> _logger;
+    private MailBox _mailbox = new MailBox();
 
+    private void Reset()
+    {
+        _mailbox = new MailBox();
+    }
+
+    public void SetSenderEmail(string username, string address)
+    {
+        _mailbox.Sender = new MailboxAddress(Encoding.Unicode, username , address);
+    }
+
+    public void AddReceiverAddress(string username, string address)
+    {
+        _mailbox.Recipients.Add(new MailboxAddress(Encoding.Unicode, username , address));
+    }
+    public void SetMessageBody(string content)
+    {
+        _mailbox.Body = content;
+    }
+    public void SetMessageSubject(string content)
+    {
+        _mailbox.Subject = content;
+    }
+
+    public void SetMessage(string subject, string body)
+    {
+        _mailbox.Body = body;
+        _mailbox.Subject = subject;
+    }
+
+    public void AddFile(string path, string subType)
+    {
+        _mailbox.BridgeFiles.Add(new BridgeMimePart()
+        {
+            Path = path,
+            SubType = subType
+        });
+    }
+    
+    public void AddFile(string path, string mediaType, string subType, ContentEncoding encoding)
+    {
+        _mailbox.BridgeFiles.Add(new BridgeMimePart()
+        {
+            Encoding = encoding,
+            Path = path,
+            MediaType = mediaType,
+            SubType = subType
+        });
+    }
+    
+    
     public EmailService(ILoggerFactory loggerFactory)
     {
+        Reset();
         _logger = loggerFactory.CreateLogger<EmailService>();
     }
     
-    //TODO: Implements the builder pattern
+    //TODO: This file will replaced "SendMailAsync" method
+    public Task<bool> BuildAndSendAsync()
+    {
+        throw new NotImplementedException();
+    }
+    
+    //TODO: Delete this file
     public async Task<bool> SendEmailAsync(string subject, string body, string toName, string toAddress, string path, SmtpServerConfiguration config)
     {
         var receiverAddress = new MailboxAddress(name: toName, address: toAddress);
@@ -24,7 +84,7 @@ public class EmailService : IEmailService
         message.From.Add(new MailboxAddress(config.Alias, config.UserHost));
         message.To.Add(receiverAddress);
         message.Subject = subject;
-        
+
         await using var stream = File.OpenRead(path);
         var attachments = new MimePart("application", "pdf")
         {
