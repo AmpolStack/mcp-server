dcc6a2f9a83ba5f75e1091d53e0b634836274e7b
Amapola <htrsanbl@gmail.com>
2025-04-16 23:59:15 -0500

    ci: include static class for generate mcp applications (templates)

--- McpServerly/Program.cs
+++ McpServerly/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Diagnostics.Metrics;
+using McpServerly.Setup;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -18,52 +19,32 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var builder = Host.CreateApplicationBuilder(args);
-        var appsettingsPath = "appsettings.json";
-        if (args.Length > 0)
+        var host = McpServerApplication.Run(args, builder =>
         {
-            if (!string.IsNullOrEmpty(args[0]))
-            {
-                appsettingsPath = $"{args[0]}/" + appsettingsPath;
-            }
-        }
-        builder.Configuration.AddJsonFile(appsettingsPath, optional: false, reloadOnChange: true);
-        builder.Logging.AddConsole(opts =>
-        {
-            opts.LogToStandardErrorThreshold = LogLevel.Trace;
+            var mongoConfig = new MongoConfiguration();
+            builder.Configuration.GetSection("Databases:mongo").Bind(mongoConfig);
+            
+            builder.Services
+                .AddSingleton<IMongoClient>(prov =>
+                {
+                    var mongoClient = new MongoClient(mongoConfig.GetConnectionString());
+                    return mongoClient;
+                })
+                .AddSingleton<IMongoDatabase>(prov =>
+                {
+                    var client = prov.GetRequiredService<IMongoClient>();
+                    return client.GetDatabase(mongoConfig.Database);
+                })
+                //Since this project is a test project, I don't really think it will include any other repository.
+                .AddScoped<IGenericRepository, ClientRepository>()
+                .AddScoped<IHtmlGeneratorService, HtmlGeneratorService>()
+                .AddScoped<IPdfGeneratorService, PdfGeneratorService>()
+                //Probably in the future this dependency has to change its lifetime to scoped
+                .AddSingleton<IEmailService, EmailService>()
+                .AddSingleton<IMailPacker, MailPack>()
+                .Configure<SmtpServerConfiguration>(builder.Configuration.GetSection("SmtpServer"))
+                .Configure<ResourceFiles>(builder.Configuration.GetSection("ResourceFiles"));
         });
-
-        var mongoConfig = new MongoConfiguration();
-        builder.Configuration.GetSection("Databases:mongo").Bind(mongoConfig);
-        builder.Services
-            .AddSingleton<IMongoClient>(prov =>
-            {
-                var mongoClient = new MongoClient(mongoConfig.GetConnectionString());
-                return mongoClient;
-            })
-            .AddSingleton<IMongoDatabase>(prov =>
-            {
-                var client = prov.GetRequiredService<IMongoClient>();
-                return client.GetDatabase(mongoConfig.Database);
-            })
-            //Since this project is a test project, I don't really think it will include any other repository.
-            .AddScoped<IGenericRepository, ClientRepository>()
-            .AddScoped<IHtmlGeneratorService, HtmlGeneratorService>()
-            .AddScoped<IPdfGeneratorService, PdfGeneratorService>()
-            //Probably in the future this dependency has to change its lifetime to scoped
-            .AddSingleton<IEmailService, EmailService>()
-            .AddSingleton<IMailPacker, MailPack>()
-            .Configure<SmtpServerConfiguration>(builder.Configuration.GetSection("SmtpServer"))
-            .Configure<ResourceFiles>(builder.Configuration.GetSection("ResourceFiles"));
-        
-        builder.Services
-            .AddLogging(opts => opts.AddConsole())
-            .AddMcpServer()
-            .WithStdioServerTransport()
-            .WithToolsFromAssembly();
-        
-        
-        var host = builder.Build();
         
         await host.RunAsync();
         
---
+++ McpServerly/Setup/McpServerApplication.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace McpServerly.Setup;
+
+public static class McpServerApplication
+{
+    public static IHost Run(string[] args, Action<HostApplicationBuilder> configure)
+    {
+        //Create de Dependency injector for default application
+        var builder = Host.CreateApplicationBuilder(args);
+        
+        var appsettingsPath = "appsettings.json";
+        
+        //because is necessary for the variable path of the appsettings.json
+        if (args.Length > 0)
+        {
+            if (!string.IsNullOrEmpty(args[0]))
+            {
+                appsettingsPath = $"{args[0]}/" + appsettingsPath;
+            }
+        }
+        
+        //Include json file for configurations and enviroment
+        builder.Configuration.AddJsonFile(appsettingsPath, optional: false, reloadOnChange: true);
+        builder.Logging.AddConsole(opts =>
+        {
+            opts.LogToStandardErrorThreshold = LogLevel.Trace;
+        });
+        
+        //Inject required services for run mcp server
+        builder.Services
+            .AddLogging(opts => opts.AddConsole())
+            .AddMcpServer()
+            .WithStdioServerTransport()
+            .WithToolsFromAssembly();
+        
+        //Inject the custom dependencies
+        configure(builder);
+
+        return builder.Build();
+        
+    }
+}
