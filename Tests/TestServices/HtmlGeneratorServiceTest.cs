e953880c3451e10b23ffd0c577405eef6b31479c
Amapola <htrsanbl@gmail.com>
2025-04-22 15:35:18 -0500

    test: includes other html generator test

--- Tests/TestServices/HtmlGeneratorServiceTest.cs
+++ Tests/TestServices/HtmlGeneratorServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
 using Moq;
 using Services.Implementations;
 
@@ -6,16 +7,20 @@ namespace Tests.TestServices;
 
 public class HtmlGeneratorServiceTest
 {
+
+    private static Mock<ILoggerFactory> ComposeFactoryMock()
+    {
+        var factory = new Mock<ILoggerFactory>();
+        var logger = new Mock<ILogger<HtmlGeneratorService>>();
+        factory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+        return factory;
+    }
+    
     [Fact]
     public void WhenTheInputIsNull()
     {
         //Arrange
-        var factory = new Mock<ILoggerFactory>();
-        var logger = new Mock<ILogger<HtmlGeneratorService>>();
-        factory
-            .Setup(f => f.CreateLogger(It.Is<string>(s => s.Contains(nameof(HtmlGeneratorService)))))
-            .Returns(logger.Object);
-        
+        var factory = ComposeFactoryMock();
         var service = new HtmlGeneratorService(factory.Object);
         
         //Act
@@ -24,5 +29,26 @@ public class HtmlGeneratorServiceTest
         Assert.Empty(resp);
         factory.Verify(f => f.CreateLogger(It.Is<string>(s => s.Contains(nameof(HtmlGeneratorService)))), Times.Once);
     }
-    
+
+
+    [Theory]
+    [InlineData("hello", "<p>hello</p>\n")]
+    [InlineData("*i*", "<p><em>i</em></p>\n")]
+    [InlineData("**b**", "<p><strong>b</strong></p>\n")]
+    [InlineData("`x`", "<p><code>x</code></p>\n")]
+    [InlineData("[l](u)", "<p><a href=\"u\">l</a></p>\n")]
+    public void ProveInputs(string md, string html)
+    {
+        //Arrange
+        var factory = ComposeFactoryMock();
+        var service = new HtmlGeneratorService(factory.Object);
+        
+        //Act
+        var resp = service.GenerateFromMarkdownString(md);
+        
+        //Assert
+        Assert.NotEmpty(resp);
+        Assert.NotNull(resp);
+        Assert.Equal(resp, html);
+    }
 }
